ac2a4d3c8aaaf4868a962cedef7bcc296d104b3c
Adam Hass <ahass@applink.io>
2020-06-21 01:05:36 -0500

    initial files

--- src/DeliveryClient.php
+++ src/DeliveryClient.php
@@ -1,29 +1,32 @@
-<?php namespace AppLinkIO\Delivery;
+<?php 
+namespace AppLinkIO\Delivery;
 
-/**
-*  A sample class
-*
-*  Use this section to define what this class is doing, the PHPDocumentator will use this
-*  to automatically generate an API documentation using this information.
-*
-*  @author yourname
-*/
+
 class DeliveryClient {
+    private $sdkVersion = '0.0.1';
 
-   /**  @var string $m_SampleProperty define here what this variable is for, do this for every instance variable */
-   private $m_SampleProperty = '';
- 
-  /**
-  * Sample method 
-  *
-  * Always create a corresponding docblock for each method, describing what it is for,
-  * this helps the phpdocumentator to properly generator the documentation
-  *
-  * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
-  *
-  * @return string
-  */
-   public function method1($param1){
-			return "Hello World";
-   }
+    public function __construct($token = null) {
+
+    } 
+
+    public function getContentItems($params = null) {
+      
+      return "getContentItems";
+    } 
+
+    public function getContentItem($item_id, $params = null) {
+      
+      return "getContentItem";
+    }
+
+    public function getAssets($params = null) {
+      
+      return "getContentItems";
+    }
+
+    public function getAsset($asset_id, $params = null) {
+      
+      return "getAsset";
+    }
+
 }
